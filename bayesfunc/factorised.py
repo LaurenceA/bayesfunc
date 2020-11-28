2f9627fa9046728cb8659ffa5e11e1ba40c296aa
Laurence Aitchison <laurence.aitchison@gmail.com>
2020-11-28 11:58:14 +0000

    fac

--- bayesfunc/factorised.py
+++ bayesfunc/factorised.py
@@ -72,15 +72,27 @@ class FactorisedConv2dWeights(FactorisedParam):
 class FactorisedLinear(AbstractLinear):
     r"""Applies a linear transformation to the incoming data: :math:`y = xA^T + b`
 
-This module supports
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        bias: If set to ``False``, the layer will not learn an additive bias.
+            Default: ``True``
 
-* sdf
-* sdf
-* sdf
+    Shape:
+        - Input: :math:`(N, *, H_{in})` where :math:`*` means any number of
+          additional dimensions and :math:`H_{in} = \text{in\_features}`
+        - Output: :math:`(N, *, H_{out})` where all but the last dimension
+          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
 
-Arguments
---------
-
+    Attributes:
+        weight: the learnable weights of the module of shape
+            :math:`(\text{out\_features}, \text{in\_features})`. The values are
+            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
+            :math:`k = \frac{1}{\text{in\_features}}`
+        bias:   the learnable bias of the module of shape :math:`(\text{out\_features})`.
+                If :attr:`bias` is ``True``, the values are initialized from
+                :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
+                :math:`k = \frac{1}{\text{in\_features}}`
     Examples::
 
         >>> m = nn.Linear(20, 30)
