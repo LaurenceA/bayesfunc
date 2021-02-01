584d0f8b9ffa7b6841a93d301085078c3dfd0d3f
Laurence Aitchison <laurence.aitchison@gmail.com>
2021-02-01 10:09:46 +0000

    Fixed det

--- bayesfunc/det.py
+++ bayesfunc/det.py
@@ -17,22 +17,23 @@ Can configure:
 
 
 class DetParam(nn.Module):
-    def __init__(self, in_features, out_features, unit_std=True, **kwargs):
+    def __init__(self, in_features, out_features, **kwargs):
         super().__init__()
         self.in_features = in_features
-        self.std      = 1. if     unit_std else 1./math.sqrt(in_features)
-        self.out_mult = 1. if not unit_std else 1./math.sqrt(in_features)
-        self.weights = nn.Parameter(self.std * t.randn(out_features, in_features))
+        self.out_mult = 1./math.sqrt(in_features)
+        self.weights = nn.Parameter(t.randn(out_features, in_features))
 
     def forward(self, Xi):
         S = Xi.shape[0]
 
         w = self.weights.expand(S, *self.weights.shape)
-        Pw = Normal(0., self.std)
+        Pw = Normal(0., 1.)
         _range = [*range(1, len(w.shape))]
         logP = Pw.log_prob(w).sum(_range)
         assert logP.shape == t.Size([S])
-        return self.out_mult*w, logP
+
+        self.logpq = logP
+        return self.out_mult*w
 
     def inducing_init(self, init):
         pass
